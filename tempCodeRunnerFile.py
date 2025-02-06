42635eb879f32b7b86087d8d1a36346109b7faaf
Jonathan Qiu <76549705+underdog1118@users.noreply.github.com>
2025-02-05 23:49:44 -0800

    Delete tempCodeRunnerFile.py

--- tempCodeRunnerFile.py
+++
@@ -1,4 +0,0 @@
-        items = [Item("Aged Brie", 5, 10)]
-        gilded_rose = GildedRose(items)
-        gilded_rose.update_quality()
-        assert items[0].quality == 11, "Aged Brie quality should increase by 1"
