e0050322d32df043107ee283d1088b8fff3208f1
Jonathan Qiu <76549705+underdog1118@users.noreply.github.com>
2025-02-05 21:42:23 -0800

    Update test_gilded_rose.py

--- test_gilded_rose.py
+++ test_gilded_rose.py
@@ -1,16 +1,22 @@
-# -*- coding: utf-8 -*-
-import unittest
-
-from gilded_rose import Item, GildedRose
-
-
 class GildedRoseTest(unittest.TestCase):
     def test_foo(self):
         items = [Item("foo", 0, 0)]
+    # example of test that checks for logical errors
+    def test_sulfuras_should_not_decrease_quality(self):
+        items = [Item("Sulfuras", 5, 80)]
         gilded_rose = GildedRose(items)
         gilded_rose.update_quality()
         self.assertEquals("fixme", items[0].name)
+        sulfuras_item = items[0]
+        self.assertEquals(80, sulfuras_item.quality)
+        self.assertEquals(4, sulfuras_item.sell_in)
+        self.assertEquals("Sulfuras", sulfuras_item.name)
+    # example of test that checks for syntax errors
+    def test_gilded_rose_list_all_items(self):
+        items = [Item("Sulfuras", 5, 80)]
+        gilded_rose = GildedRose(items)
+        all_items = gilded_rose.get_items()
+        self.assertEquals(["Sulfuras"], all_items)
 
 
 if __name__ == '__main__':
-    unittest.main()
